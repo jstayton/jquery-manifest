9b12c46048be1c08ce27cae50f7e439e51f11c6e
Justin Stayton <justin.stayton@gmail.com>
2011-10-05 21:23:11 -0500

    Initial build scripts.

---
+++ build/minify.js
@@ -0,0 +1,71 @@
+module.exports = function (callback) {
+  var fs = require('fs'),
+      http = require('http'),
+      path = require('path'),
+      qs = require('querystring'),
+      util = require('util'),
+      src = fs.readFileSync(path.normalize(__dirname + '/../src/jquery.manifest.js'), 'utf8'),
+      body = {},
+      options = {},
+      request;
+
+  body = qs.stringify({
+    js_code: src,
+    compilation_level: 'SIMPLE_OPTIMIZATIONS',
+    output_info: 'compiled_code',
+    output_format: 'text'
+  });
+
+  options = {
+    host: 'closure-compiler.appspot.com',
+    path: '/compile',
+    method: 'POST',
+    headers: {
+      'Content-Length': body.length,
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }
+  };
+
+  process.stdout.write('Sending jquery.manifest.js to Google Closure Compiler... ');
+
+  request = http.request(options, function (response) {
+    var widgetSrc = fs.readFileSync(path.normalize(__dirname + '/../lib/jquery.ui.widget.min.js'), 'utf8'),
+        marcoPoloSrc = fs.readFileSync(path.normalize(__dirname + '/../lib/jquery.marcopolo.min.js'), 'utf8'),
+        minifiedSrc = fs.createWriteStream(path.normalize(__dirname + '/../jquery.manifest.min.js')),
+        srcHeader = /^\/\*[\s\S]*?\*\//.exec(src);
+
+    process.stdout.write("Done!\n");
+
+    response.setEncoding('utf8');
+
+    process.stdout.write('Writing jquery.ui.widget.min.js to jquery.manifest.min.js... ');
+
+    minifiedSrc.write(widgetSrc + "\n");
+
+    process.stdout.write("Done!\n");
+
+    process.stdout.write('Writing jquery.marcopolo.min.js to jquery.manifest.min.js... ');
+
+    minifiedSrc.write(marcoPoloSrc + "\n");
+
+    process.stdout.write("Done!\n");
+
+    process.stdout.write('Writing minified jquery.manifest.js to jquery.manifest.min.js... ');
+
+    minifiedSrc.write(srcHeader + "\n");
+
+    util.pump(response, minifiedSrc);
+
+    process.stdout.write("Done!\n");
+
+    callback && callback();
+  });
+
+  request.on('error', function (error) {
+    process.stdout.write("Failed!\n");
+    console.log(error.message);
+  });
+
+  request.write(body);
+  request.end();
+};
